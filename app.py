77e83e7c900dc92826e2be38ed4b21afb7bfd8d1
mx.zi <125572060+fahrezi93@users.noreply.github.com>
2025-08-12 18:33:24 +0700

    Clean up unused files and simplify model structure

--- app.py
+++ app.py
@@ -23,12 +23,9 @@ CORS(app, resources={r"/*": {"origins": ALLOWED_ORIGINS}})
 # --- Konfigurasi ---
 # Daftar model untuk dicoba, dalam urutan prioritas
 MODELS = [
-    './waste_model.h5',
-    './waste_model_trained.h5',
-    '/app/waste_model.h5',
-    '/app/waste_model_trained.h5',
-    'server/waste_model.h5',
-    'server/waste_model_trained.h5'
+    'waste_model_final.h5',  # Model yang akan digunakan
+    './waste_model_final.h5',
+    '/app/waste_model_final.h5'
 ]
 MODEL_URL = os.environ.get('MODEL_URL', '')  # URL untuk download model
 CORRECTIONS_DIR = 'corrections' # Folder untuk menyimpan gambar koreksi
